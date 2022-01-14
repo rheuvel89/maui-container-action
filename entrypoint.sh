7c9d2e1624c05eb906a47924a9e7712c7b4d5c01
rheuvel <rheuvel@sogyo.nl>
2022-01-14 19:55:02 +0100

    Parameters for source location and signing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-msbuild /github/workspace/$1 /p:AndroidSdkDirectory=/usr/lib/android-sdk /p:Configuration="Release" /p:Platform="Any CPU" /restore
-msbuild /github/workspace/$2 /p:AndroidSdkDirectory=/usr/lib/android-sdk /p:Configuration="Release" /p:Platform="Any CPU" /t:restore /t:rebuild /t:PackageForAndroid /p:OutputPath="/github/workspace/build/"
-msbuild /github/workspace/$2 /p:AndroidSdkDirectory=/usr/lib/android-sdk /p:Configuration="Release" /p:Platform="Any CPU" /t:SignAndroidPackage /p:OutputPath="/github/workspace/build/"
+# msbuild /github/workspace/$1 /p:AndroidSdkDirectory=/usr/lib/android-sdk /p:Configuration="Release" /p:Platform="Any CPU" /restore
+# msbuild /github/workspace/$2 /p:AndroidSdkDirectory=/usr/lib/android-sdk /p:Configuration="Release" /p:Platform="Any CPU" /t:restore /t:rebuild /t:PackageForAndroid /p:OutputPath="/github/workspace/build/"
+# msbuild /github/workspace/$2 /p:AndroidSdkDirectory=/usr/lib/android-sdk /p:Configuration="Release" /p:Platform="Any CPU" /t:SignAndroidPackage /p:OutputPath="/github/workspace/build/"
+export SLN_PATH=$1
+export CSPROJ_PATH=$2
+export CONFIGURATION=$3
+export ALIAS=$4
+export SIGNING_KEY_PASS=$5
+export SIGNING_KEY=$6
+export KEY_STORE_PASSWORD=$7
+
+echo  -n "$SIGNING_KEY" | base64 --decode --output android.keystore
+
+msbuild "$CSPROJ_PATH" /t:restore /verbosity:normal /t:Rebuild /t:SignAndroidPackage /p:Configuration="$CONFIGURATION" /p:Platform="Any CPU" /p:AndroidKeyStore=true /p:AndroidSigningKeyAlias="$ALIAS" /p:AndroidSigningKeyPass="$SIGNING_KEY_PASS" /p:AndroidSigningKeyStore="android.keystore" /p:AndroidSigningStorePass="$KEY_STORE_PASSWORD"
