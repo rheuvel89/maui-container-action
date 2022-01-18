bf1cf8b32c1d5f83e6c2cf8eeeb77a85ee199e88
rheuvel89 <46789097+rheuvel89@users.noreply.github.com>
2022-01-18 20:05:59 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,13 @@
 export SLN_PATH=$1
 export CSPROJ_PATH=$2
 export CONFIGURATION=$3
-export ALIAS=$4
-export SIGNING_KEY_PASS=$5
-export SIGNING_KEY=$6
-export KEY_STORE_PASSWORD=$7
+export PLATFORM=$3
+export ALIAS=$5
+export SIGNING_KEY_PASS=$6
+export SIGNING_KEY=$7
+export KEY_STORE_PASSWORD=$8
 
 echo  -n "$SIGNING_KEY" | base64 --decode >> /github/workspace/android.keystore
 
-msbuild "$CSPROJ_PATH" /p:AndroidSdkDirectory=/usr/lib/android-sdk /p:Configuration="$CONFIGURATION" /p:Platform="Any CPU" /restore
-msbuild "$CSPROJ_PATH" /t:restore /verbosity:normal /t:Rebuild /t:SignAndroidPackage /p:AndroidSdkDirectory=/usr/lib/android-sdk /p:Configuration="$CONFIGURATION" /p:Platform="Any CPU" /p:AndroidKeyStore=true /p:AndroidSigningKeyAlias="$ALIAS" /p:AndroidSigningKeyPass="$SIGNING_KEY_PASS" /p:AndroidSigningKeyStore="/github/workspace/android.keystore" /p:AndroidSigningStorePass="$KEY_STORE_PASSWORD" /p:OutputPath="/github/workspace/build/"
+msbuild "$CSPROJ_PATH" /p:AndroidSdkDirectory=/usr/lib/android-sdk /p:Configuration="$CONFIGURATION" /p:Platform="$PLATFORM" /restore
+msbuild "$CSPROJ_PATH" /t:restore /verbosity:normal /t:Rebuild /t:SignAndroidPackage /p:AndroidSdkDirectory=/usr/lib/android-sdk /p:Configuration="$CONFIGURATION" /p:Platform="$PLATFORM" /p:AndroidKeyStore=true /p:AndroidSigningKeyAlias="$ALIAS" /p:AndroidSigningKeyPass="$SIGNING_KEY_PASS" /p:AndroidSigningKeyStore="/github/workspace/android.keystore" /p:AndroidSigningStorePass="$KEY_STORE_PASSWORD" /p:OutputPath="/github/workspace/build/"
